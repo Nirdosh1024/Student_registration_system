e81059f3f86f1433195af05a3f28430afec4a2ca
pushparaj21 <singhpushparaj31@gmail.com>
2023-03-24 14:02:33 +0530

    fixed the mongodb instance creation from excel

--- Models/studentModel.js
+++ Models/studentModel.js
@@ -1,30 +1,33 @@
-const mongoose =  require("mongoose");
+/** @format */
+
+const mongoose = require("mongoose");
 
 const newStudent = mongoose.Schema({
-     
-    ID: {
-        type:Number,
-        required: true
-    },
+  ID: {
+    type: Number,
+    required: true,
+  },
 
-    Name: {
-        type: String,
-        required: true
-    },
+  Name: {
+    type: String,
+    required: true,
+  },
 
-    Email: {
-        type: String,
-        required: true
-    },
+  Email: {
+    type: String,
+    required: true,
+  },
 
-    Branch: {
-        type: String,
-        required: true
-    },
+  Branch: {
+    type: String,
+    required: true,
+  },
 
-    Rank: {
-        type: Number,
-        required: true
-    }
-})
+  Rank: {
+    type: Number,
+    required: true,
+  },
+});
 
+const newStudentModel = mongoose.model("newStudent", newStudent);
+module.exports = newStudentModel;
--- app.js
+++ app.js
@@ -1,108 +1,121 @@
+/** @format */
+
 const express = require("express");
 const mongoose = require("mongoose");
 const multer = require("multer");
 const cors = require("cors");
 const expressLayouts = require("express-ejs-layouts");
-const app = express()
-const exceltoJson = require("convert-excel-to-json")
-const path = require("path")
+const app = express();
+const exceltoJson = require("convert-excel-to-json");
+const path = require("path");
 
-app.use(cors({
-    origin:"http://localhost:3000/",
-    credentials: true
-}))
+// model imported
+const newStudentModel = require("./Models/studentModel");
+
+app.use(
+  cors({
+    origin: "http://localhost:3000/",
+    credentials: true,
+  })
+);
 
 app.use(expressLayouts);
-app.set("view engine","ejs");
-
-
+app.set("view engine", "ejs");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-
+app.use(express.urlencoded({ extended: true }));
 
 // multer setup to upload files to the server
 const storage = multer.diskStorage({
-    destination: (req, file , cb) => {
-        cb(null, "uploads")
-    }, 
-    filename: (req, file , cb)=> {
-        const { originalname } = file
-        cb(null,originalname); 
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    const { originalname } = file;
+    cb(null, originalname);
+  },
+});
 const upload = multer({ storage });
 
-
-app.post('/',(req,res) =>{
-    console.log(req)
-    res.status(201).json({
-            "statustell":"4000"
-    })
+app.post("/", (req, res) => {
+  console.log(req);
+  res.status(201).json({
+    statustell: "4000",
+  });
 });
 
-
-
-app.get('/',(req,res) =>{
-    res.render('layout')
-}
-
-);
+app.get("/", (req, res) => {
+  res.render("layout");
+});
 
 app.post("/upload", upload.single("excel"), (req, res) => {
-    if(!req.file) {
-        res.json({"status": "NOT OKAY"});
-    } else {
-        const filepath = path.join(__dirname,"/uploads",`${req.file.filename}`)
-       importExceltoJson(filepath);
-        res.json({"status": "OKAY"});
-    }
+  if (!req.file) {
+    res.json({ status: "NOT OKAY" });
+  } else {
+    const filepath = path.join(__dirname, "/uploads", `${req.file.filename}`);
+    importExceltoJson(filepath);
+    res.json({ status: "OKAY" });
+  }
 });
- function importExceltoJson(filepath){
-    const exceldata = exceltoJson({
-        sourceFile: filepath,
+function importExceltoJson(filepath) {
+  const exceldata = exceltoJson({
+    sourceFile: filepath,
 
-        sheets:[{
-            name:newstudent ,
+    sheets: [
+      {
+        name: "Sheet1",
 
         header: {
-            rows: 1
-        } ,   
-        
-        columnTokey: {
-            A: 'ID',
-            B: 'name',
-            C: 'email',
-            D: 'mobileno',
-            E: 'branch',
-            F: 'rank'
-        }
+          rows: 1,
+        },
 
-        }] });
- 
-console.log(exceldata);
-// insert data in db
+        columnToKey: {
+          A: "ID",
+          B: "Name",
+          C: "Email",
+          D: "Mobileno",
+          E: "Branch",
+          F: "Rank",
+        },
+      },
+    ],
+  });
 
-mongoose.connect('mongodb://localhost/myapp',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(()=> {console.log("Mongo DB Connected....")
-let dbo = db.db("exceldb");
-    dbo.collection("newstudent").insertMany(exceldata.Newstudent, (err, res) => {
-        if(err)  err;
-        console.log("NUmber of data inserted: " + res.insertedCount);
+  console.log(exceldata);
+  // insert data in db
 
-        db.close();
+  mongoose
+    .connect("mongodb://localhost/myapp", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Mongo DB Connected....");
+      insertData();
+    })
+    .catch((e) => {
+      //   console.error(e);
+      console.log("The error is when connected", e);
     });
-}
-,(err)=>{
-    console.log(err);
-}
-);
 
-};
-const PORT = process.env.PORT || 5000
-app.listen(PORT,() => {
-    console.log("Server has started at 5000")
-})
+  function insertData() {
+    console;
+    newStudentModel
+      .create([...exceldata.Sheet1])
+      .then((Data) => {
+        console.log(Data);
+        mongoose.connection.close().then(() => {
+          console.log("connection closed");
+        });
+      })
+      .catch((e) => {
+        //   console.error(e);
+
+        console.log("The error is on insert ", e);
+      });
+  }
+}
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server has started at 5000");
+});
