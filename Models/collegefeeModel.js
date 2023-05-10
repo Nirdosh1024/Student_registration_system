4e15c5a8e026102132692602a684c5af1c1bfa1a
Nirdosh4096 <hmrnirdoshchauhan@gmail.com>
2023-05-10 13:25:55 +0530

    model and db implemented

--- Models/collegefeeModel.js
+++ Models/collegefeeModel.js
@@ -1,28 +1,30 @@
 const mongoose = require("mongoose");
 
 const collegefee = mongoose.Schema({
-  year: {
-    type: String,
-    academic_fee: {
-      type: Number,
-      required: true,
-    },
-
-    hostel_fee: { type: Number, required: true },
-
-    mess_fee: {
-      type: Number,
-      required: true,
-    },
-    mess_security_fee: {
-      type: Number,
-    },
-
-    maintenance_fee: {
-      type: Number,
-      required: true,
-    },
-  },
+      year: {
+        type: String,
+        required: true
+      },
+      academic_fee: {
+        type: Number,
+        required: true
+      },
+      maintenance_fee: {
+        type: Number,
+        required: true
+      },
+      hostel_fee: {
+        type: Number,
+        required: true
+      },
+      mess_fee: {
+        type: Number,
+        required: true
+      },
+      mess_security_fee: {
+        type: Number,
+        required: true
+      }
 });
 
 const collegefeeModel = mongoose.model("collegefeeModel", collegefee);
--- public/index.js
+++ public/index.js
@@ -1,23 +1,23 @@
-// const personalForm = document.querySelector(".personal-form");
+const personalForm = document.querySelector(".personal-form");
 
 
-// personalForm.addEventListener("submit", async () => {
-//     const dataInForm = new FormData(personalForm);
-//     const values = [...dataInForm];
-//     const formDataObject = {};
-//     values.forEach((ele) => {
-//         formDataObject[ele[0]] = ele[1];
-//     })
-//     console.log(formDataObject);
-//     const axiosResponse = await axios({
-//         method: "POST",
-//         url: "http://localhost:5000/form-submit",
-//         data: formDataObject
-//     });
+personalForm.addEventListener("submit", async () => {
+    const dataInForm = new FormData(personalForm);
+    const values = [...dataInForm];
+    const formDataObject = {};
+    values.forEach((ele) => {
+        formDataObject[ele[0]] = ele[1];
+    })
+    console.log(formDataObject);
+    const axiosResponse = await axios({
+        method: "POST",
+        url: "http://localhost:5000/form-submit",
+        data: formDataObject
+    });
 
-//     console.log(axiosResponse);
+    console.log(axiosResponse);
 
-// })
+})
 
 // Function for admin fee form submission
 
